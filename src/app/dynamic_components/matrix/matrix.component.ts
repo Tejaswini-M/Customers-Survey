6a6642cbf8d40e6ae439725dd2346cfe46e2cad6
Tejaswini M <tejaswini.m@optus.com.au>
2021-04-14 18:08:31 +1000

    matrix component

--- src/app/dynamic_components/matrix/matrix.component.ts
+++ src/app/dynamic_components/matrix/matrix.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatrixComponent implements OnInit {
 
+  muls=[
+    { id:1, value:'',status:null},
+    { id:2, value:'',status:null},
+    { id:3, value:'',status:null}
+  ];
+  
+  model = {
+    ques:'',
+    muls:[
+      { id:1, value:'',status:null},
+      { id:2, value:'',status:null},
+      { id:3, value:'',status:null}
+    ]
+  }
+  submitted = false;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {   
   }
 
+    addTable() {
+      const obj =  { id:this.model.muls.length+1 , value:'', status:null};
+      this.model.muls.push(obj);
+    }
+
+    deleteRow(i:any) {
+      this.model.muls.splice(i,1);
+    }
+ 
+  onSubmit() {
+    console.log("value");
+    this.submitted =true;
+  }
+
+  drop(event: CdkDragDrop<any[]>){
+    //this.viewContainerRef.move(this.components[event.previousIndex].hostView, event.currentIndex);
+    moveItemInArray(this.model.muls, event.previousIndex, event.currentIndex);
+    //console.log(event.container.data);
+  }
+
+  status = ['Strongly agree', 'agree', 'Somewhat agree', 'Neither agree nor disagree', 'Somewhat disagree', 'Disagree', 'Strongly disagree'];
 }
