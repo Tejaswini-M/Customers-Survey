3629dcd74014681f59a736f105afc435eada903e
Tejaswini M <tejaswini.m@optus.com.au>
2021-05-10 16:35:02 +1000

    n

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -12,6 +12,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { ConfigService } from '../services/config.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CompType } from '../comp-type'; 
+import { PlatformLocation } from '@angular/common';
 
 @Component({
   selector: 'app-question',
@@ -110,19 +111,38 @@ export class QuestionComponent implements OnInit{
   viewContainerRef:any;
   //component: ComponentRef<any>[]=[];
   constructor(public fb: FormBuilder,private rout: Router, private route: ActivatedRoute,
-    public config:ConfigService,private compFactoryResolver: ComponentFactoryResolver) {
-     window.location.hash="no-back-button";
+    public config:ConfigService,private compFactoryResolver: ComponentFactoryResolver, private location: PlatformLocation) {
+    this.location.onPopState(() => {        
+       alert("Changes you made may not be saved.");
+       //window.location.href = window.location.href + '/qns';
+       console.log(window.location.href);
+      
+        //return false;
+    });//window.location.hash="no-back-button";
+
      //window.location.hash="Again-No-back-button";//again because google chrome don't insert first hash into history
-     window.onhashchange=function(){confirm("Changes you made may not be saved.")}
+     //window.onhashchange=function(){confirm("Changes you made may not be saved.")}
   }
   
   @HostListener("window:beforeunload", ["$event"]) unloadHandler(event: Event) {
     let result = confirm("Changes you made may not be saved.");
     if (result) {
+      console.log("Clicked on refresh button");
       //history.back();
     } 
         event.returnValue = false; // stay on same page
   }
+
+  // @HostListener('window:popstate', ['$event'])
+  // onPopState(event: Event) {
+  //   console.log('Back button pressed');
+  //   let result = confirm("Changes you made may not be saved.");
+  //   if (result) {
+  //     //history.back();
+  //   } 
+  //       event.returnValue = false;
+    
+  // }
  
 
 
