a116e47be55180a85a3e012e1e5397a426740aba
tejaswini m <tejaswini.m@optus.com.au>
2021-05-12 11:25:51 +0530

    button color modification

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -44,9 +44,7 @@ export class HomeComponent implements OnInit {
     //   this.config.userValues=this.config.allComps[index].list;
     // }
   }
-  viewDesc(index : any) {
-    
-  }
+  
   viewAdmin(index : any){
     //this.config.userResponse=false;
     //this.config.adminResponse=true;
